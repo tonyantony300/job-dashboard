84396e6151e0c0eb34f05c809303391ee5725f98
tony <tonyantony300@gmail.com>
2024-05-05 17:29:26 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="filter-section">
+        <span className="filter">Roles</span>
+        <span className="filter">Number of Employees</span>
+        <span className="filter">Experience</span>
+        <span className="filter">Remote</span>
+        <span className="filter">Minimum Base Pay Salary</span>
+        <span className="filter">Search Company Name</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
